a270031322deddb5247f40d344bbef8cc8f88eea
waihinsoe <waihinsoe332004@gmail.com>
2023-02-11 22:59:53 +0630

    first commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+const apiUrl = process.env.API_URL;
+
+app.use(express.static("public"));
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script type="text/javascript">
+        localStorage.setItem('apiUrl', '${apiUrl}')
+        window.location.href ='/';
+    </script>
+  </body>
+</html>
+`;
+
+app.get("/api", (req: Request, res: Response) => {
+  res.send(html);
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  res.send({ name: "wai", email: "wai@gmail.com" });
+});
+
+app.listen(9000, () => {
+  console.log("Server is listening at port 9000");
+});
